27586128d61d9f5e0eb1253ca0bff874fadae374
Alex Gusev <alex@flancer64.com>
2019-04-17 15:22:20 +0300

    Restore previous fix (is better then current).

--- Plugin/Magento/Framework/Mail/Message.php
+++ Plugin/Magento/Framework/Mail/Message.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Process recipients emails and replace its with developer's emails.
+ * Process recipients emails and replace its by developer's emails.
  *
  * Authors: Alex Gusev <alex@flancer64.com>
  * Since: 2018
@@ -13,10 +13,14 @@ class Message
 
     /** @var \Flancer32\EmailHijack\Helper\Config */
     private $hlpConfig;
+    /** @var \Magento\Framework\App\ProductMetadataInterface */
+    private $metadata;
 
     public function __construct(
+        \Magento\Framework\App\ProductMetadataInterface $metadata,
         \Flancer32\EmailHijack\Helper\Config $hlpConfig
     ) {
+        $this->metadata = $metadata;
         $this->hlpConfig = $hlpConfig;
     }
 
@@ -28,11 +32,8 @@ class Message
     ) {
         $enabled = $this->hlpConfig->getHijackEnabled();
         if ($enabled) {
-            /*
-             *  convert input to universal form
-             *      - \Zend\Mail\Message::addTo($emailOrAddressOrList, $name = null)
-             *          - \Zend\Mail\AddressList::add($emailOrAddress, $name = null)
-             * */
+            $useDirect = $this->useDirectOrder();
+            /* convert input to universal form (see \Zend_Mail::addTo) */
             if (!is_array($email)) {
                 $email = array($name => $email);
             }
@@ -41,30 +42,47 @@ class Message
             /* replace original emails by developers ones */
             $replaced = [];
             /* compose recipient name from all emails (from one email in the most cases) */
-            $allNamesAsOne = '';
+            $allNames = '';
             foreach ($email as $nameOrig => $emailOrig) {
                 $part = trim("$nameOrig::$emailOrig");
                 $part = str_replace('@', '.at.', $part);
-                $allNamesAsOne .= "$part ";
+                $allNames .= "$part ";
             }
-            $allNamesAsOne = trim($allNamesAsOne); // remove trailing space
+            $allNames = trim($allNames); // remove trailing space
 
             /* compose array with developers emails, add original recipients as a name */
-            foreach ($addrsDev as $devEmail) {
-                /* starting from M2.3 (Zend2) we need compose [email=>name] array instead of [$name=>$email] */
-                $replaced[trim($devEmail)] = trim($allNamesAsOne);
+            foreach ($addrsDev as $key => $one) {
+                $newEmail = trim($one);
+                if ($useDirect) {
+                    $replaced["_{$key}_{$allNames}"] = $newEmail;
+                } else {
+                    $replaced[$newEmail] = $allNames;
+                }
             }
             /**
              * Result in common case:
              *
-             * dev1@mail.com => "Customer First::cust1.at.gmail.com Customer Second::cust2.at.mail.com",
-             * dev2@mail.com => "Customer First::cust1.at.gmail.com Customer Second::cust2.at.mail.com"
+             * dev1@mail.com => Customer First::cust1.at.gmail.com Customer Second::cust2.at.mail.com,
+             * dev2@mail.com => Customer First::cust1.at.gmail.com Customer Second::cust2.at.mail.com
              */
             /* call parent method with replaced argument */
             $result = $proceed($replaced);
         } else {
             /* call parent method with original arguments */
             $result = $proceed($email, $name);
+        }
+        return $result;
+    }
+
+    /**
+     * Use [email => name] or [name => email]
+     */
+    private function useDirectOrder()
+    {
+        $result = true;
+        $version = $this->metadata->getVersion();
+        if ($version >= '2.3.') {
+            $result = false;
         }
         return $result;
     }
